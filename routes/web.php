4ad91bef1b1f11ac82c7a683cddad848649d4530
Rohan-shah7 <rohanshah2090@gmail.com>
2025-08-04 19:18:55 +0545

    add route

--- routes/web.php
+++ routes/web.php
@@ -3,38 +3,44 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 
-Route::prefix('student')->group(function () {
-    Route::get('/ranjit', [StudentController::class, 'ranjit'])->name('student.ranjit');
-    Route::get('/salok', [StudentController::class, 'salok'])->name('student.salok');
-    Route::get('/rohan', [StudentController::class, 'rohan'])->name('student.rohan');
-    Route::get('/rupesh', [StudentController::class, 'rupesh'])->name('student.rupesh');
-    Route::get('/prabin', [StudentController::class, 'prabin'])->name('student.prabin');
-    Route::get('/suhana', [StudentController::class, 'suhana'])->name('student.suhana');
-    Route::get('/adish', [StudentController::class, 'adish'])->name('student.adish');
-    Route::get('/aman', [StudentController::class, 'aman'])->name('student.aman');
+
+
+Route::prefix('student')->controller(StudentController::class)->group(function () {
+    Route::get('ranjit', 'ranjit')->name('student.ranjit');
+    Route::get('salok', 'salok')->name('student.salok');
+    Route::get('rohan', 'rohan')->name('student.rohan');
+    Route::get('rupesh', 'rupesh')->name('student.rupesh');
+    Route::get('prabin', 'prabin')->name('student.prabin');
+    Route::get('suhana', 'suhana')->name('student.suhana');
+    Route::get('adish', 'adish')->name('student.adish');
+    Route::get('aman', 'aman')->name('student.aman');
 });
 
 
 
-// use App\Http\Controllers\CollegueController;
 
-// Route::get('/collegue', [CollegueController::class, 'showByPhone']);
+
+
+ use App\Http\Controllers\CollegueController;
+use App\Http\Controllers\RohanController;
+
+//  Route::get('/collegue', [CollegueController::class, 'showByPhone']);
 
 // // Home route
 // Route::get('/', [RohanController::class, 'home'])->name('home');
 
 // // About route
-// Route::get('/about', [RohanController::class, 'about'])->name('about');
+//  Route::get('/about', [RohanController::class, 'about'])->name('about');
 
 // // Login route
-// Route::get('/login', [RohanController::class, 'login'])->name('login');
-// // Register route
-// Route::get('/register', [RohanController::class, 'register'])->name('register');
+//  Route::get('/login', [RohanController::class, 'login'])->name('login');
+//  // Register route
+//  Route::get('/register', [RohanController::class, 'register'])->name('register');
 
-// // Contact route
-// Route::get('/contact', [RohanController::class, 'contact'])->name('contact');
-// // Team route
-// Route::get('/team', [RohanController::class, 'team'])->name('team');
+//  // Contact route
+//  Route::get('/contact', [RohanController::class, 'contact'])->name('contact');
+//  // Team route
+//  Route::get('/team', [RohanController::class, 'team'])->name('team');
 
 
 
